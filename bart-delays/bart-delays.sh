9451472bbb0953438add85965bd175f38c5f6801
mradambeck <mr.adambeck@gmail.com>
2017-02-07 09:06:53 -0800

    adds logging

--- bart-delays/bart-delays.sh
+++ bart-delays/bart-delays.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 t timeline sfbartalert -n 10 --csv > ~/scripts/bart-delays/timeline.csv
 
-TODAYSDATE=$(date '+%y'-'%m'-'%d')
-THISHOURSEARCH=$(grep -E 20$TODAYSDATE.*' '`date '+%H':` ~/scripts/bart-delays/timeline.csv)
-LASTHOURSEARCH=$(grep -E 20$TODAYSDATE.*' '`date -v -1H '+%H:'` ~/scripts/bart-delays/timeline.csv)
+TODAYSDATE=$(date '+%y'-'%m'-'%d') > ~/scripts/bart-delays/date-test.log
+THISHOURSEARCH=$(grep -E 20$TODAYSDATE.*' '`date '+%H':` ~/scripts/bart-delays/timeline.csv) > ~/scripts/bart-delays/thishousearch-test.log
+LASTHOURSEARCH=$(grep -E 20$TODAYSDATE.*' '`date -v -1H '+%H:'` ~/scripts/bart-delays/timeline.csv) > ~/scripts/bart-delays/lasthousearch-test.log
 
 BODY="https://twitter.com/SFBARTalert :: "
 
@@ -25,3 +25,4 @@ curl -XPOST https://api.twilio.com/2010-04-01/Accounts/ACff0e5a19d48a7721c2b64da
     --data-urlencode "To=$MY_NUMBER" \
     --data-urlencode "From=$TWILIO_NUMBER" \
     -u $TWILIO_ID':'$TWILIO_SECRET
+    > ~/scripts/bart-delays/twilio-test.log
--- cron-file.md
+++ cron-file.md
@@ -1,7 +1,13 @@
 For help: http://www.techradar.com/how-to/computing/apple/terminal-101-creating-cron-jobs-1305651
 
 ```
-15  10  *  *  * bash ~/scripts/clear-screenshots/clear-screenshots.sh
-15  5 * * * bash ~/scripts/clear-temp-folder/clear-temp.sh
-0 5 * * 1-5 bash ~/scripts/bart-delays/bart-delays.sh
+# For bart-delays.sh:
+TWILIO_ID="XXXXXXXXXX"
+TWILIO_SECRET="XXXXXXXXXX"
+TWILIO_NUMBER="+1##########"
+MY_NUMBER="+1##########"
+
+15      10      *       *       *       bash ~/scripts/clear-screenshots/clear-screenshots.sh >~/scripts/clear-screenshots/stdout.log 2>~/scripts/clear-screenshots/stderr.log
+15      5       *       *       *       bash ~/scripts/clear-temp-folder/clear-temp.sh >~/scripts/clear-temp-folder/stdout.log 2>~>/scripts/clear-temp-folder/stderr.log
+*/5     *       *       *       *       bash ~/scripts/bart-delays/bart-delays.sh >/scripts/bart-delays/stdout.log 2>/scripts/bart-delays/stderr.log
 ```
